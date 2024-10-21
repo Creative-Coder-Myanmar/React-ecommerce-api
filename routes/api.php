59a9f83b45f59e456e00565f82edc9902bf1d192
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-10-21 11:03:53 +0700

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,19 @@
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-
+//auth api
 Route::post('/users', [UserController::class, 'store']);
 Route::post('/login', [UserController::class, 'login']);
-Route::post('/users/{user}/profile-update', [UserController::class, 'profileImageUpdate'])->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-//role api
-Route::post('/roles', [RoleController::class, 'store']);
-Route::delete('/roles/{role}', [RoleController::class, 'delete']);
 
 //category api
 Route::get('/categories', [CategoryController::class, 'show']);
@@ -31,6 +29,7 @@ Route::get('/products/{product}', [ProductController::class, 'show']);
 Route::post('/products', [ProductController::class, 'store'])->middleware(['auth:sanctum', AdminMiddleware::class]);
 Route::put('/products/{product}', [ProductController::class, 'update'])->middleware(['auth:sanctum', AdminMiddleware::class]);
 Route::delete('/products/{product}', [ProductController::class, 'delete'])->middleware(['auth:sanctum', AdminMiddleware::class]);
+Route::post('/products/{product}/update-image', [ProductController::class, 'imageUpdate'])->middleware(['auth:sanctum', AdminMiddleware::class]);
 
 //order api
 Route::get('/orders', [OrderController::class, 'show']);
@@ -38,8 +37,3 @@ Route::get('/orders/{order}', [OrderController::class, 'detail']);
 Route::post('/orders', [OrderController::class, 'store'])->middleware(['auth:sanctum', AdminMiddleware::class]);
 Route::put('/orders/{order}', [OrderController::class, 'update'])->middleware(['auth:sanctum', AdminMiddleware::class]);
 Route::delete('/orders/{order}', [OrderController::class, 'delete'])->middleware(['auth:sanctum', AdminMiddleware::class]);
-
-Route::post('/products/{product}/update-image', [ProductController::class, 'imageUpdate'])->middleware(['auth:sanctum', AdminMiddleware::class]);
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
